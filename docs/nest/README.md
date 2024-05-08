3d0ccf89468df7442c87eff2a6867cb11d0ce0b7
Ray259 <elninothepea@gmail.com>
2024-05-08 23:46:03 +0700

    chore: add docs

---
+++ docs/nest/README.md
@@ -0,0 +1,13 @@
+# Compodoc documentation
+
+Generate docs
+```sh
+npm run docs:generate
+```
+
+Start docs
+```sh
+npm run docs:start
+```
+
+Open localhost:9000
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 declare const module: any;
 const PORT = process.env.PORT || 3000;
-const VER = process.env.API_VERSION || 'api';
+const VER = process.env.API_VERSION || 'api/v1';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(VER);
   app.use(cookieParser());
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Petcare service API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(PORT);
 
   if (module.hot) {
